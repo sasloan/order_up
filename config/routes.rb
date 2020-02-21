eb6c1a88abd84487c634892deda442e853633d53
sasloan <sebastiansloan94@gmail.com>
2020-02-21 10:09:16 -0700

    Complete user story 3

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 	#dishes
 
 	get '/dishes/:dish_id', to: 'dishes#show'
+
+	#chefs
+
+	get '/chefs/:chef_id', to: 'chefs#show'
+
+	#chef_ingredients
+
+	get '/chefs/:chef_id/ingredients', to: 'chef_ingredients#index'
 end
