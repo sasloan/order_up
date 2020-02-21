fcbb768e61f3212e26c12d4e7eb354a711587333
sasloan <sebastiansloan94@gmail.com>
2020-02-21 09:17:29 -0700

    Many to many relationship created and set up

--- spec/models/ingredients_spec.rb
+++ spec/models/ingredients_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Ingredients, type: :model do
+RSpec.describe Ingredient, type: :model do
 	describe 'Validations' do
 		it {should validate_presence_of :name}
 		it {should validate_presence_of :calories}
